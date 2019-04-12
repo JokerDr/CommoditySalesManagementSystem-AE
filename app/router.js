abb142e543dbe6dd418190fefab223556cd4c368
yuanqinglong <yuanql96@yeah.net>
2019-04-13 03:05:48 +0800

    use EggJs 框架开发接口

---
+++ app/router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
+  router.resources('session', '/api/v1/session', controller.v1.session);
+};
